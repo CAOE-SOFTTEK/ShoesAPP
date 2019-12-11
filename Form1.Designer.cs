4e8bfc34c63f94222ebd100a3e2a9f54b00b1b8c
Curso <Curso@LSTKMY231257.stktrainning.com>
2019-12-11 11:21:14 -0600

    Codigo de buscar y agregue librerias para mvc

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Observations = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PriceDistributor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PriceClient = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(601, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Nombre,
+            this.Description,
+            this.Observations,
+            this.PriceDistributor,
+            this.PriceClient});
+            this.dataGridView1.Location = new System.Drawing.Point(41, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 272);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Titulo";
+            this.Title.Name = "Title";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Descripcion";
+            this.Description.Name = "Description";
+            // 
+            // Observations
+            // 
+            this.Observations.HeaderText = "Observaciones";
+            this.Observations.Name = "Observations";
+            // 
+            // PriceDistributor
+            // 
+            this.PriceDistributor.HeaderText = "P.Distribuidor";
+            this.PriceDistributor.Name = "PriceDistributor";
+            // 
+            // PriceClient
+            // 
+            this.PriceClient.HeaderText = "P.Cliente";
+            this.PriceClient.Name = "PriceClient";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(823, 464);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Observations;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PriceDistributor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PriceClient;
+        private System.Windows.Forms.Button button2;
     }
 }
 
